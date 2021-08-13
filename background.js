416d071bd859c35c36f4d4e000bd72cf742dfdd3
garywill <garywill@disroot.org>
2021-08-13 09:00:03 +0800

    1.2

--- background.js
+++ background.js
@@ -1,31 +1,50 @@
-(async () => {
-        
-    function setButtonText(documentFontEnabled) {
-        if (!documentFontEnabled) {
-            browser.browserAction.setBadgeBackgroundColor({ color: "#00BF00" });
-            browser.browserAction.setBadgeText({ text: "R" });
-        } else {
-            browser.browserAction.setBadgeBackgroundColor({ color: "#dd0000" });
-            browser.browserAction.setBadgeText({ text: "no R" });
-        }
+
+async function updateIcon()
+{
+    var read_enabled = (await browser.browserSettings.useDocumentFonts.get({})).value;
+    setButtonText(read_enabled);
+}
+function setButtonText(documentFontEnabled) {
+    if (!documentFontEnabled) {
+        browser.browserAction.setBadgeBackgroundColor({ color: "#00BF00" });
+        browser.browserAction.setBadgeText({ text: "" });
+    } else {
+        browser.browserAction.setBadgeBackgroundColor({ color: "#dd0000" });
+        browser.browserAction.setBadgeText({ text: "no R" });
     }
+}
 
+
+browser.browserAction.setBadgeTextColor({ color: "#FFFFFF" });
+
+//var read_enabled_onload = (await browser.browserSettings.useDocumentFonts.get({})).value;
+//setButtonText(read_enabled_onload);
+
+updateIcon();
+
+browser.browserAction.onClicked.addListener(async () => {
+    var read_enabled_onclick = (await browser.browserSettings.useDocumentFonts.get({})).value;
+    var set_enabled = !read_enabled_onclick ; 
     
-    browser.browserAction.setBadgeTextColor({ color: "#FFFFFF" });
+    await browser.browserSettings.useDocumentFonts.set({ value: set_enabled });
     
-    var read_enabled_onload = (await browser.browserSettings.useDocumentFonts.get({})).value;
-    setButtonText(read_enabled_onload);
+    //var read_enabled_afterclick = (await browser.browserSettings.useDocumentFonts.get({})).value;
+    //setButtonText(read_enabled_afterclick);
+    updateIcon();
+});
 
+browser.browserSettings.useDocumentFonts.onChange.addListener(function() { 
+    //console.log("browserSettings change event");
+    updateIcon();
+});
 
+browser.tabs.onUpdated.addListener(function() { 
+    //console.log("tabs.onUpdated  event");
+    updateIcon();
+});
 
-    browser.browserAction.onClicked.addListener(async () => {
-        var read_enabled_onclick = (await browser.browserSettings.useDocumentFonts.get({})).value;
-        var set_enabled = !read_enabled_onclick
-        
-        browser.browserSettings.useDocumentFonts.set({ value: set_enabled });
-        
-        var read_enabled_afterclick = (await browser.browserSettings.useDocumentFonts.get({})).value;
-        setButtonText(read_enabled_afterclick);
-    });
+browser.windows.onFocusChanged.addListener(function() { 
+    //console.log("window focus change event");
+    updateIcon();
+});
 
-})();
